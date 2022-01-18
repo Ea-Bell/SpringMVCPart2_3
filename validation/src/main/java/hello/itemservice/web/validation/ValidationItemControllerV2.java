c7e5df09c2aaf4c8c3c6d311348f9720b92da2da
Ea-Bell <cjp975@naver.com>
2022-01-18 17:55:27 +0900

    검증1 - Vaildation
    오류 코드와 메시지 처리6
    
    검증 오류 코드는 다음과 같이 2가지로 나눌 수 있다.
     * 개발자가 직접 설정한 오류 코드 -> rejectValue()를 직접 호출
     * 스프링이 직접 검증 오류에 추가한 경우(주로 타입정보가 맞지 않음)
    
     price 필드에 문자 "A"를 입력해보자.
     로그를 확인해보면 BindingResult 에 FieldError 가 담겨있고, 다음과 같은 메시지 코드들이 생성된
     것을 확인할 수 있다.
     codes[typeMismatch.item.price,typeMismatch.price,typeMismatch.java.lang.Integer,typeMismatch]
    
     다음과 같이 4가지 메시지 코드가 입력되어 있다.
     typeMismatch.item.price
     typeMismatch.price
     typeMismatch.java.lang.Integer
     typeMismatch

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -187,6 +187,12 @@ public class ValidationItemControllerV2 {
         log.info("objectName = {}", bindingResult.getObjectName());
         log.info("target = {}", bindingResult.getTarget());
 
+        if(bindingResult.hasErrors()){
+            log.info("error={}",bindingResult);
+            return "validation/v2/addForm";
+        }
+
+
         //검증 로직
         //ValidationUtils.rejectIfEmptyOrWhitespace(bindingResult, "itemName", "required");
         if(!StringUtils.hasText(item.getItemName())){
