e359a46bba220b7488bc7949a94c9cf861f277fd
Ea-Bell <cjp975@naver.com>
2022-01-18 23:32:03 +0900

    검증1 - Vaildation
    Validator 분리2
    
    @IniteBinder
    WebDataBinder는 @IniteBinder가 포함된 컨트롤러를 실행할때마다 검증을 실시함.
    
    WebDataBinder 에 검증기를 추가하면 해당 컨트롤러에서는 검증기를 자동으로 적용할 수 있다.
    @InitBinder 해당 컨트롤러에만 영향을 준다. 글로벌 설정은 별도로 해야한다.
    
    Test코드로 BeanValidation코드를 짜봄

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -10,14 +10,13 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
 import java.util.List;
+
 
 @Slf4j
 @Controller
