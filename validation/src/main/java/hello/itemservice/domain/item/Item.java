21b6754622c6cdcb5ab23170abb43da7545e5fa7
Ea-Bell <cjp975@naver.com>
2022-01-19 13:06:58 +0900

    검증2 - Bean Validations
    Bean Validation 한계
    
    사이드이펙트 발생 -> 발생요인 Item의 id에 @NotNull추가 quantity의 @Max주석
    
    등록시 화면이 넘어가지 않으면서 다음과 같은 오류를 볼 수 있다.
    'id': rejected value [null];
    왜냐하면 등록시에는 id 에 값이 없다. 따라서 @NotNull id 를 적용한 것 때문에 검증에 실패하고 다시 폼 화면으로 넘어온다. 결국 등록 자체도 불가능하고, 수량 제한도 걸지 못한다.

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 //@ScriptAssert(lang = "javascript", script = "_this.price *  _this.quantity>=10000", message = "총합 10000원이 넘게 입력해주세요")
 public class Item {
 
+    @NotNull //수정 요구사항
     private Long id;
     @NotBlank(message = "공백XXX")
     private String itemName;
@@ -20,7 +21,8 @@ public class Item {
     @Range(min = 1000, max = 1000000)
     private Integer price;
     @NotNull
-    @Max(9999)private Integer quantity;
+    //@Max(9999) //수정 요구사항
+    private Integer quantity;
     public Item() {
     }
     public Item(String itemName, Integer price, Integer quantity) {
